45e214bdf69ec9347652707ca30988247d488608
Shreya Tiwari <st1736773@gmail.com>
2026-03-23 23:07:13 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -3,23 +3,28 @@ import os
 
 KEY_FILE = "secret.key"
 
-
 # -------------------------------
-# GENERATE & SAVE KEY
+# GENERATE / LOAD KEY
 # -------------------------------
 def generate_key():
+    """
+    Generate a key if not exists and save to file
+    """
     if not os.path.exists(KEY_FILE):
         key = Fernet.generate_key()
         with open(KEY_FILE, "wb") as f:
             f.write(key)
 
-
 # -------------------------------
 # LOAD KEY
 # -------------------------------
 def load_key():
-    return open(KEY_FILE, "rb").read()
-
+    """
+    Load the Fernet key from file
+    """
+    with open(KEY_FILE, "rb") as f:
+        key = f.read()
+    return key
 
 # -------------------------------
 # ENCRYPT PASSWORD
@@ -27,15 +32,12 @@ def load_key():
 def encrypt_password(password):
     key = load_key()
     f = Fernet(key)
-    encrypted = f.encrypt(password.encode())
-    return encrypted.decode()
-
+    return f.encrypt(password.encode()).decode()
 
 # -------------------------------
 # DECRYPT PASSWORD
 # -------------------------------
-def decrypt_password(encrypted_password):
+def decrypt_password(enc_password):
     key = load_key()
     f = Fernet(key)
-    decrypted = f.decrypt(encrypted_password.encode())
-    return decrypted.decode()
+    return f.decrypt(enc_password.encode()).decode()
